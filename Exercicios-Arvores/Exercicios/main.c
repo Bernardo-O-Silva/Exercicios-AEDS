424c86a7526aadb0505db01e4fcb19e881aab0b4
Bernardo-O-Silva <2021951558@teiacoltec.org>
2022-07-09 00:12:06 +0000

    Done on Gitpod

---
+++ Exercicios-Arvores/Exercicios/main.c
@@ -0,0 +1,44 @@
+/*Exercício 2 - Implemente uma inserção em árvore binária onde elementos menores que
+a raiz ficam esquerda e elementos maiores que a raiz ficam direita.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "arvore.h"
+#define SUCESSO 0
+
+int main(int argc, char** argv){
+
+    Arv* arvorinha;
+    char charUser;
+
+    arvorinha = arv_criavazia();
+    
+    while (charUser != '0'){
+
+        printf("Digite um caractere para inserir na árvore (0 finaliza a inserção): ");
+        scanf("%c", &charUser);
+        getchar();
+
+        if (charUser != '0'){
+
+            arvorinha = arv_insere_maior_menor(arvorinha, charUser);
+
+        }
+
+    }
+
+    printf("Árvore:\n");
+    arv_imprime_simetrica(arvorinha);
+    printf("\n");
+    /*
+    arv_imprime_preordem(arvorinha);
+    printf("\n");
+    arv_imprime_posordem(arvorinha);
+    printf("\n");
+    */
+
+    arv_libera(arvorinha);
+    return SUCESSO;
+}
